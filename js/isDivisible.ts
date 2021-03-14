64286b09bdb7d5a702bab911f3265e3b83822466
Luis Manresa <luis.manresa@fabrick-io.com>
2021-03-14 16:58:34 +0100

    trouver le plus petit diviseur

--- js/isDivisible.ts
+++ js/isDivisible.ts
@@ -6,8 +6,28 @@ function isDivisible(a: number, divisor:number) {
 let a = [14,18,21,49];
 let divisor = 7;
 
-function foreignNumber(tab:number[], divisor: number) {
+function familyNumbers(tab:number[], divisor: number) {
     return tab.filter(x => isDivisible(x, divisor));
 }
 
-console.log(foreignNumber(a, divisor));
+function intruders(tab:number[], divisor: number) {
+    let intruders = [];
+    for(const a of tab) {
+        if(isDivisible(a, divisor)) intruders.push(a);
+    }
+    return intruders;
+}
+
+let b = [21,14,49];
+function findComunDivisor(tab: number[]): number {
+    const countDivisors = (acc: number, currentValue: number) => {
+        if(!isDivisible(currentValue, divisor)) acc += 1;
+        return acc;
+    };
+    let divisor = 2;
+    do {
+        if(tab.reduce(countDivisors, 0) === 0) return divisor;
+        divisor += 1;
+    }while(divisor <= 30);
+    return -1;
+}
